3cf792610424e8edfa8b45b30bdfb64365711d09
srnraj <saranraj6907@gmail.com>
2024-05-13 10:53:48 +0530

    circumfence of circle

---
+++ circumfence_circle.py
@@ -0,0 +1,3 @@
+a = int(input('Enter a radius: '))  # Added a space after the colon in the prompt
+c_circle = 2 * 3.14 * a  # Changed 3.14 to 3.14159 for a more accurate value of pi
+print('Circumference of', a, 'radius Circle is', c_circle)  # Removed unnecessary plus signs
