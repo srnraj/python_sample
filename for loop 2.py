5b83fddd1e73f6cd4864f61849f1e7191b35791f
srnraj <saranraj6907@gmail.com>
2024-05-21 17:35:36 +0530

    a python program using for loop 2

---
+++ for loop 2.py
@@ -0,0 +1,8 @@
+# FOR Loop with range() - Iterates a specific number of times
+for i in range(3):  # Range(start, stop, step) - Here, 3 iterations
+  print(f"Loop: {i}")
+
+# Nested Loops - Loops within loops
+for i in range(2):
+  for j in range(3):
+    print(f"({i}, {j})")  # Print coordinates
